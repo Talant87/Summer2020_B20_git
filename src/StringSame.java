5ba340ab2898706b976ca8bebf4df70f43dd2b51
talant87 <barcaman87@gmail.com>
2020-08-07 14:59:36 -0400

    update StringSame.java

--- src/StringSame.java
+++ src/StringSame.java
@@ -1,2 +1,22 @@
+import java.util.Arrays;
+
 public class StringSame {
+    public static void main(String[] args) {
+        String a = "abc";
+        String b = "baca";
+        char[] ch1 = a.toCharArray();
+        char[] ch2 = b.toCharArray();
+
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+
+        String a1="", a2="";
+        for(char each: ch1){
+            a1 += each;
+        }
+        for(char each: ch2) {
+            a2 += each;
+        }
+        System.out.println(a1.equals(a2)) ;
+    }
 }
